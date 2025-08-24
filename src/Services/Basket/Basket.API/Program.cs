6540da6d655a56adae21f36805f766c366b39d21
Jennifer Lee <Jenny.L.Lee@outlook.com>
2025-08-24 15:03:34 -0500

    Update Basket API for .NET 8.0 and Redis caching
    - Target .NET 8.0 in `Basket.API.csproj` and add health check packages.
    - Modify `Dockerfile` to expose ports 8080 and 8081; streamline build process.
    - Enhance `Program.cs` with Redis caching and health check services.
    - Update `launchSettings.json` to disable browser launch and adjust URLs.
    - Add Redis connection string in `appsettings.json`.
    - Define Redis service in `docker-compose.override.yml` and update ports.
    - Include new `basket.api` service in `docker-compose.yml`.
    - Create `Dockerfile.original` for build and publish stages.
    - Implement `CachedBasketRepository` for efficient caching of shopping cart data.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
@@ -16,13 +19,35 @@ builder.Services.AddMarten(opts =>
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName); // Set UserName as the PK
 }).UseLightweightSessions();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository> ();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+//Add distributed cache, but can't duplicate method on line 19 so we use manual regirstration if you cant use Scrutor
+//builder.Services.AddScoped<IBasketRepository>(provider =>
+//{
+//    var basketRepository = provider.GetRequiredService<BasketRepository>();
+//    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+//});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.MapHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
